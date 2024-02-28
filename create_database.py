44e29a5a2bd76292acd68782d11482fe5ef7d24c
Yash Patel <yash.patel0419@gmail.com>
2024-02-28 17:56:20 -0600

    Initial Commit

---
+++ create_database.py
@@ -0,0 +1,4 @@
+from langchain_community.document_loaders import DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema import Document
+from langchain_openai import OpenAIEmbeddings
